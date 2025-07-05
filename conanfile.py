d98ae6a098911d2a2c15777bbf57790aeeef9312
NSOFT <nsoft.sagaonline@gmail.com>
2025-07-05 13:02:30 +0300

    Update conanfile to Conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,18 @@
-from conans import ConanFile, CMake
-from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version, check_min_cppstd, load
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.scm import Version
+from conan.tools.build import check_min_cppstd
 import re
-
+import os
 
 def get_version():
     try:
-        content = load("CMakeLists.txt")
+        with open("CMakeLists.txt") as f:
+            content = f.read()
         version = re.search(r"^\s*project\(ozo\s+VERSION\s+([^\s)]+)", content, re.M).group(1)
         return version.strip()
     except Exception:
         return None
-
 
 class OzoConan(ConanFile):
     name = "ozo"
@@ -20,58 +21,55 @@ class OzoConan(ConanFile):
     topics = ("ozo", "yandex", "postgres", "postgresql", "cpp17", "database", "db", "asio")
     url = "https://github.com/yandex/ozo"
     description = "Conan package for yandex ozo"
-    settings = "os", "compiler"
+    settings = "os", "compiler", "build_type", "arch"
 
-    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "LICENCE", "AUTHORS"
+    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "LICENSE", "AUTHORS"
 
-    generators = "cmake_find_package"
-    requires = ("boost/1.74.0", "resource_pool/0.1.0", "libpq/13.1")
+    generators = "CMakeDeps", "CMakeToolchain"
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
+    requires = (
+        "boost/1.79.0",
+        "resource_pool/cci.20210322",
+        "libpq/15.12"
+    )
 
-    def configure(self):
-        if self.settings.os == "Windows":
-            raise ConanInvalidConfiguration("OZO is not compatible with Winows")
-        if self.settings.compiler.get_safe("cppstd"):
-            check_min_cppstd(self, "17")
+    def layout(self):
+        cmake_layout(self)
+
+    def validate(self):
+        #if self.settings.os == "Windows":
+        #    raise Exception("OZO is not compatible with Windows")
+        if self.settings.get_safe("compiler.cppstd"):
+            check_min_cppstd(self, 17)
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()  # аналог header_only()
 
     def package_info(self):
-        self.cpp_info.components["_ozo"].includedirs = ["include"]
-        self.cpp_info.components["_ozo"].requires = ["boost::boost", "boost::system", "boost::thread", "boost::coroutine",
-                                                     "resource_pool::resource_pool", # == elsid::resource_pool in cmake
-                                                     "libpq::pq",                    # == PostgreSQL::PostgreSQL in cmake
-                                                     ]
-        self.cpp_info.components["_ozo"].defines = [
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.requires = [
+            "boost::boost", "boost::system", "boost::thread", "boost::coroutine",
+            "resource_pool::resource_pool", "libpq::pq"
+        ]
+        self.cpp_info.defines = [
             "BOOST_COROUTINES_NO_DEPRECATION_WARNING",
             "BOOST_HANA_CONFIG_ENABLE_STRING_UDL",
             "BOOST_ASIO_USE_TS_EXECUTOR_AS_DEFAULT"
         ]
 
         compiler = self.settings.compiler
-        version = Version(compiler.version)
-        if compiler == "clang" or compiler == "apple-clang" or (compiler == "gcc" and version >= 9):
-            self.cpp_info.components["_ozo"].cxxflags = [
+        version = Version(str(compiler.version))
+        if str(compiler) in ["clang", "apple-clang"] or (str(compiler) == "gcc" and version >= "9"):
+            self.cpp_info.cxxflags = [
                 "-Wno-gnu-string-literal-operator-template",
                 "-Wno-gnu-zero-variadic-macro-arguments",
             ]
-
-        self.cpp_info.filenames["cmake_find_package"] = "ozo"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "ozo"
-        self.cpp_info.names["cmake_find_package"] = "yandex"
-        self.cpp_info.names["cmake_find_package_multi"] = "yandex"
-        self.cpp_info.components["_ozo"].names["cmake_find_package"] = "ozo"
-        self.cpp_info.components["_ozo"].names["cmake_find_package_multi"] = "ozo"
